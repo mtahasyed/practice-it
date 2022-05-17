0ee8ced28ed54eb1898f55179e9d9e0fde4ee403
Mohammad Taha <81573515+mtahasyed@users.noreply.github.com>
2022-05-17 00:08:08 -0700

    Update randomLines.java

--- chapter-5/randomLines.java
+++ chapter-5/randomLines.java
@@ -6,17 +6,16 @@
 
 public static void randomLines() {
     Random random = new Random();
-    int line = random.nextInt(5) + 5;
-    
-    for (int i = 0; i < line; i++) {
+    int numString = random.nextInt(5) + 5;
+    for (int i = 0; i < numString; i++) {
         int numOfChar = 0;
-        
-        while (numOfChar <= 0) {
+        int numOfChar = random.nextInt(80) + 1;
+        int counter = 1;
+        while (numOfChar != counter) {
             int letter = random.nextInt(26) + 97;
-            System.out.print((char)letter);
-            numOfChar++;
+            System.out.print((char) letter);
+            counter++;
         }
-        
         System.out.println();
     }
 }
